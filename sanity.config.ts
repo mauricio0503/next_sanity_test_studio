02f18e4f27dd6e8e851427a12757fcb31ddc5652
malforsaja-dev <malfor.saja.dev@gmail.com>
2025-08-11 12:44:06 +0200

    feat: bootstrap sanity studio

---
+++ sanity.config.ts
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'crypto-casinos-us',
+
+  projectId: 'dor153n9',
+  dataset: 'production',
+
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
